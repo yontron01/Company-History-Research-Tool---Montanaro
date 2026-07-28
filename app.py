698f7dd03192356e3c0b47863a625dc2ba62374b
Yonis Abdi Aziz <yonisabdiaziz05@gmail.com>
2026-07-28 15:43:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ if button:
 
     wiki_wiki = wikipediaapi.Wikipedia(user_agent = 'Company History Research Tool (yonisabdiaziz05@gmail.com)', language = 'en')
     page_py = wiki_wiki.page(company_name)
-    status = st.spinner('Wikipedia Page - Exists: %s' % page_py.exists())
-    time.sleep(0.1)
+    st.write('Wikipedia Page - Exists: %s' % page_py.exists())
+    
     
 
     #page_missing = wiki_wiki.page('NonExistingPageWithStrangeName')
@@ -84,8 +84,8 @@ if button:
     if page_py.exists() == False:
         st.write(f"{company_name} not found on Wikipedia")
         st.write("---")
-        status = st.spinner("Checking elsewhere...")
-        time.sleep(0.1)
+        st.write("Checking elsewhere...")
+        
         
 
     from ddgs import DDGS
@@ -99,8 +99,8 @@ if button:
         st.write(f"Error message: [e]")
 
     urls = []
-    status = st.spinner("Storing URLs...")
-    time.sleep(0.1)
+    st.write("Storing URLs...")
+    
     
     for r in results:
         urls.append(r["href"])
@@ -109,8 +109,8 @@ if button:
     import trafilatura
 
     info = []
-    status = st.spinner("Downloading information from URLs...")
-    time.sleep(0.1)
+    st.write("Downloading information from URLs...")
+    
     
     info.append(page_py.text)
     for link in urls:
@@ -120,8 +120,8 @@ if button:
 
     st.write("Downloaded information")
     st.write("---")
-    status = st.spinner("Cleaning text...")
-    time.sleep(0.1)
+    st.write("Cleaning text...")
+    
     
     info_cleaned = [text for text in info if text is not None]
     info_cleaned_text = "\n".join(info_cleaned)
@@ -138,8 +138,8 @@ if button:
 
 
         client = genai.Client(api_key=api_key_input)
-        status = st.spinner("Summarising text...")
-        time.sleep(0.1)
+        st.write("Summarising text...")
+        
         
         st.write("---")
         system_prompt = f"""You are a corporate analyst investigating the company: {company_name}.
